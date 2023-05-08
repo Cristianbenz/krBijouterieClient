d06736ce84ea953209dadd4d62cee04bbaa30312
Cristianbenz <contactocbenitez@gmail.com>
2023-05-08 18:29:10 -0300

    add titles and favicon

--- src/app/routes/product.routes.ts
+++ src/app/routes/product.routes.ts
@@ -4,6 +4,6 @@ import { Route } from "@angular/router";
 
 
 export default [
-  {path: ':section/detalle/:id', component: DetailComponent},
-  {path: ':category', component: ProductsComponent},
+  {path: ':section/detalle/:id', component: DetailComponent, title: 'KrBijouterie - Detalle'},
+  {path: ':category', component: ProductsComponent, title: 'KrBijouterie - Productos'},
 ] as Route[];
