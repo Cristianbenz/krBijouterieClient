b8614b3a1b20b777f5b51ed12c1736f033b35b4b
Cristianbenz <contactocbenitez@gmail.com>
2023-05-08 17:36:10 -0300

    add footer and nav links

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,6 +1,6 @@
-import { Component, ElementRef, OnInit, ViewChild } from "@angular/core";
+import { AfterViewInit, Component, ElementRef, OnInit, ViewChild } from "@angular/core";
 import { CommonModule } from '@angular/common';
-import { Router, RouterModule } from "@angular/router";
+import { ActivatedRoute, Router, RouterModule } from "@angular/router";
 import { FormsModule } from '@angular/forms';
 
 import { CardComponent } from "../../components/categoryCard/categoryCard.component";
@@ -15,19 +15,28 @@ import { ICategory } from "src/app/models/category";
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.scss']
 })
-export class HomeComponent implements OnInit {
-  @ViewChild('categoriesSection') categoriesSection!: ElementRef;
+export class HomeComponent implements OnInit, AfterViewInit {
   public search = '';
   public categories: Array<ICategory> = []
 
   constructor(
     private _router: Router,
+    private _route: ActivatedRoute,
     private _categoryService: CategoryService
   ) { }
-
+  
   ngOnInit(): void {
     this._categoryService.getAll(false)
     .subscribe(response => this.categories = response.data)
+  }
+
+  ngAfterViewInit(): void {
+    this._route.fragment.subscribe(value => {
+      if(value) {
+        const el = document.getElementById(value)
+        el && el.scrollIntoView({'behavior': 'smooth'})
+      }
+    })
   }
 
   submit(evt: any) {
