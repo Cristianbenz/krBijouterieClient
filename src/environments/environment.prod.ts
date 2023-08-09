21b48818c76997434a3c76027d6295566ae0d8a3
Cristian Benitez <91490781+Cristianbenz@users.noreply.github.com>
2023-08-09 14:20:35 -0300

    update api rul in environtment.prod (#9)
    * add map styles, fix product page title and modify for to add product
    
    * add footer and nav links
    
    * fix fragment scroll scroll
    
    * add titles and favicon
    
    * add loader
    
    * add seo tags
    
    * Update environment.prod.ts api url
    
    * Update environment.prod.ts api url

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,5 +1,5 @@
 export const environment = {
     production: true,
-    apiUrl: "https://krbijou.onrender.com",
+    apiUrl: "https://krbijou.onrender.com/api",
     mapKey: "35608eb168c6424091205207ccc8d7c0"
 };
